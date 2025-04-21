839d8f614179d1727211f0a93355a1af80bfe4ec
SaileachStudios <coyote@coyotesdenstore.com>
2025-04-21 19:52:44 -0400

    Added HoleBehavior to handle visuals for hole.

---
+++ Assets/Scripts/Board/HoleBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaileachStudios.Mirlini.Board
+{
+    public class HoleBehavior : MonoBehaviour
+    {
+        [SerializeField] private GameObject correctIndicator;
+        [SerializeField] private GameObject incorrectIndicator;
+
+        private HoleController controller;
+
+        void Awake() {
+            controller = new HoleController();
+            controller.onHoleStatusChanged += OnStatusChanged;
+        }
+
+        void OnStatusChanged(bool isCorrect) {
+            if (isCorrect) {
+                correctIndicator.SetActive(true);
+                incorrectIndicator.SetActive(false);
+            }
+            else {
+                correctIndicator.SetActive(false);
+                incorrectIndicator.SetActive(true);
+            }
+        }
+
+        public HoleController GetHoleController() {
+            return controller;
+        }
+    }
+}
